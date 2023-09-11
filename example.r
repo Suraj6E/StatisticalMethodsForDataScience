9c0221c4c0c06b5cf28e5c10ca1cb5d3c8d1f5d3
suraj.neupane <suraj.neupane151@gmail.com>
2023-09-11 07:19:32 +0545

    vector

--- example.r
+++ example.r
@@ -1,33 +1,17 @@
 # Sample dataset: Exam scores
 scores <- c(85, 90, 78, 92, 88, 75, 82, 95, 68, 98)
 
-# Calculate mean
-mean_score <- mean(scores)
+# Create a histogram
+hist(scores, breaks = 5, col = "skyblue", xlab = "Scores", ylab = "Frequency",
+     main = "Histogram of Exam Scores")
 
-# Calculate median
-median_score <- median(scores)
-
-# Calculate range
-range_score <- max(scores) - min(scores)
-
-# Calculate variance and standard deviation
-variance_score <- var(scores)
-std_deviation <- sd(scores)
-
-# Calculate quartiles and interquartile range
-q1 <- quantile(scores, 0.25)
-q3 <- quantile(scores, 0.75)
-iqr <- q3 - q1
+# Create a scatter plot
+plot(seq_along(scores), scores, col = "darkorange", pch = 16,
+     xlab = "Student", ylab = "Scores", main = "Scatter Plot of Exam Scores")
 
 # Create a box plot
-boxplot(scores, main = "Exam Scores Distribution", ylab = "Scores")
+boxplot(scores, col = "lightgreen", xaxt = "n", ylab = "Scores",
+        main = "Box Plot of Exam Scores")
 
-# Print the calculated statistics
-cat("Mean:", mean_score, "\n")
-cat("Median:", median_score, "\n")
-cat("Range:", range_score, "\n")
-cat("Variance:", variance_score, "\n")
-cat("Standard Deviation:", std_deviation, "\n")
-cat("Q1:", q1, "\n")
-cat("Q3:", q3, "\n")
-cat("IQR:", iqr, "\n")
+# Adding x-axis labels for the box plot
+axis(1, at = 1, labels = c("Scores"))
---
+++ vector.r
@@ -0,0 +1,9 @@
+v <- c(98, 99, 100)
+print(v)
+print(v[1:2])
+
+# 1 to 10
+for (i in 1:10) {
+    print(i)
+}
+
